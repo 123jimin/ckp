42a6f2d4a66b6d1c85b138181f3df258a5276e3e
Jimin Park <pjm950@gmail.com>
2024-03-05 22:03:29 +0900

    Benchmark for `bench_find_divisor`.

--- bench/number_theory/factor.py
+++ bench/number_theory/factor.py
@@ -1,12 +1,19 @@
 from bench.util import bench
 from ckp.number_theory.factor import *
 
+def bench_find_divisor():
+    # A random prime number, chosen by a fair dice roll.
+    p = 1854622803871
+    x = pollard_rho_find_divisor(p ** 2)
+    assert x == p
+    return x
+
 def bench_factor():
-    x = sum(max(factor(r)) for r in range(1_000_000_000, 1_000_010_000))
+    x = sum(max(factor_pollard_rho(r)) for r in range(1_000_000_000, 1_000_010_000))
     assert x == 823444990958
     return x
 
 if __name__ == '__main__':
     bench([
-        "bench_factor()",
+        "bench_find_divisor()",
     ], num_trials=5, global_vars=globals())
