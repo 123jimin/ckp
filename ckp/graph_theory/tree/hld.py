9f65babaef910e9b66b24518f7cb7d585ccda0f8
Jimin Park <pjm950@gmail.com>
2024-04-22 00:00:34 +0900

    Added decompose_descendant for TreeHLD.

--- ckp/graph_theory/tree/hld.py
+++ ckp/graph_theory/tree/hld.py
@@ -1,32 +1,36 @@
 from .tree import tree_sizes
+from typing import Generator
 
 class TreeHLD:
     """ Stores information about heavy-light decomposition of a given tree. """
 
-    __slots__ = ('path_index', 'paths')
-    path_index: list[int]
+    __slots__ = ('tree', 'path_index', 'paths')
+    path_index: list[tuple[int, int]]
     paths: list[list[int]]
 
     def __init__(self, tree, sizes: list[int]|None = None):
         if sizes is None: sizes = tree_sizes(tree.neighbors, tree.root)
 
+        self.tree = tree
         self.path_index = [None] * len(tree)
         self.paths = [[]]
 
-        self._init_node(tree, sizes, 0, tree.root)
-    
-    def _init_node(self, tree, sizes: list[int], ind: int, curr: int, parent: int = -1):
-        self.path_index[curr] = ind
-        self.paths[ind].append(curr)
+        self._init_node(sizes, 0, tree.root)
 
-        neighbors = tree.neighbors[curr]
+    def __str__(self): return f"<TreeHLD path_index={self.path_index} paths={self.paths}>"
+    
+    def _init_node(self, sizes: list[int], ind: int, curr: int, parent: int = -1):
+        self.path_index[curr] = (ind, len(curr_path := self.paths[ind]))
+        curr_path.append(curr)
+
+        neighbors = self.tree.neighbors[curr]
 
         if parent != -1:
             if len(neighbors) == 1: return
             if len(neighbors) == 2:
                 for ch in neighbors:
                     if ch != parent:
-                        self._init_node(tree, sizes, ind, ch, curr)
+                        self._init_node(sizes, ind, ch, curr)
                         return
                 assert(False)
         
@@ -35,11 +39,23 @@ class TreeHLD:
             if ch == parent: continue
             if sizes[ch] == max_ch_size:
                 max_ch_size += 1
-                self._init_node(tree, sizes, ind, ch, curr)
+                self._init_node(sizes, ind, ch, curr)
             else:
                 new_ind = len(self.paths)
                 self.paths.append([])
-                self._init_node(tree, sizes, new_ind, ch, curr)
+                self._init_node(sizes, new_ind, ch, curr)
+    
+    def decompose_descendant(self, ancestor: int, node: int) -> Generator[tuple[int, int, int], None, None]:
+        """
+            Given that `ancestor` is an ancestor of `node`, returns a list of intervals in heavy paths, where each interval is given as (path_index, path_start, path_end). The interval is half-open.
+        """
+        path_index, paths, tree = self.path_index, self.paths, self.tree
+        p_ancestor, i_ancestor = path_index[ancestor]
+        p_node, i_node = path_index[node]
 
-    def __str__(self):
-        return f"<TreeHLD path_index={self.path_index} paths={self.paths}>"
+        while p_ancestor != p_node:
+            yield (p_node, 0, i_node+1)
+            p_node, i_node = path_index[node := tree.parents[paths[p_node][0]]]
+
+        assert(i_ancestor <= i_node)
+        yield (p_ancestor, i_ancestor, i_node+1)
--- test/graph_theory/tree/hld.py
+++ test/graph_theory/tree/hld.py
@@ -4,10 +4,35 @@ from ckp.graph_theory.tree.hld import *
 from ckp.graph_theory.tree import Tree
 
 class TestHLD(unittest.TestCase):
+    def _test_decompose_descendant(self, tree: Tree, hld: TreeHLD, u: int, v: int):
+        x = v
+        for (pi, ps, pe) in hld.decompose_descendant(u, v):
+            self.assertLess(pi, len(hld.paths), f"{u=} {v=} {pi=}")
+            curr_path = hld.paths[pi]
+
+            self.assertLess(ps, len(curr_path), f"{u=} {v=} {curr_path=}")
+            self.assertLess(ps, pe, f"{u=} {v=} {curr_path=}")
+            self.assertLessEqual(pe, len(curr_path), f"{u=} {v=} {curr_path=}")
+
+            self.assertEqual(x, curr_path[pe-1], f"{u=} {v=} {curr_path=} {ps=} {pe=}")
+            for i in reversed(range(ps, pe-1)):
+                x = tree.parents[x]
+                self.assertEqual(x, curr_path[i], f"{u=} {v=} {curr_path=} {ps=} {pe=} {i=}")
+            
+            if x != u:
+                x = tree.parents[x]
+
+        self.assertEqual(x, u)
+
     def test_simple(self):
         tree = Tree.from_edges([(0, 1), (0, 2), (2, 3), (2, 4), (4, 5), (1, 6), (1, 7), (7, 8), (5, 9)], root=0)
         hld = TreeHLD(tree)
+        
         self.assertEqual(len(hld.paths), 4)
         self.assertEqual(len(hld.path_index), len(tree))
+        
         self.assertSetEqual(set("".join(str(v) for v in path) for path in hld.paths), {"02459", "178", "6", "3"})
         self.assertTrue(all(ind is not None for ind in hld.path_index))
+
+        for (u, v) in [(0, 1), (0, 2), (0, 8), (0, 3), (4, 9)]:
+            self._test_decompose_descendant(tree, hld, u, v)
