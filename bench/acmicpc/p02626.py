ce7b592324b84e65d387384a26a184cc82db3653
Jimin Park <pjm950@gmail.com>
2024-05-07 23:25:52 +0900

    Added benchmark for min_enclosing_circle.

---
+++ bench/acmicpc/p02626.py
@@ -0,0 +1,19 @@
+from ckp.geometry import min_enclosing_circle
+
+import random, sys, math
+sys.setrecursionlimit(10_000_000)
+N = 100000
+
+random.seed(42)
+points = [(random.randint(-30000, 30000), random.randint(-30000, 30000)) for _ in range(N)]
+
+def bench():
+    a, (acx, acy), acr = min_enclosing_circle(points)
+
+    assert(abs(acx/a - 163.008313) < 0.001)
+    assert(abs(acy/a - 54.380628) < 0.001)
+    assert(abs(math.sqrt(acr/a/a) - 42263.568380) < 0.001)
+    
+    return a, acr
+
+tags = ['geometry', 'min_enclosing_circle']
--- bench/acmicpc/problems.py
+++ bench/acmicpc/problems.py
@@ -1,5 +1,6 @@
 from . import p01016
 from . import p01067
 from . import p02268
+from . import p02626
 from . import p11438
 from . import p12858
