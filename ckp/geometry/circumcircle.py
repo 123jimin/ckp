700eba1ea77bbc1f4795202263adf1763c6f3492
Jimin Park <pjm950@gmail.com>
2024-05-10 21:05:53 +0900

    Simplify circumcircle implementations.

--- ckp/geometry/circumcircle.py
+++ ckp/geometry/circumcircle.py
@@ -1,6 +1,6 @@
 def _determinant(a, b, c, d, e, f, g, h, i): return a*(e*i-f*h) - b*(d*i-f*g) + c*(d*h-e*g)
 
-def is_in_circumcircle(C: tuple[int|float, int|float, int|float], p: tuple[int|float, int|float]) -> bool:
+def is_in_circumcircle(C: tuple[float, tuple[float, float], float], p: tuple[float, float]) -> bool:
     """
         Given a circumcircle (closed disc) returned by `circumcircle_of_*` functions, and a point p = (x, y), returns whether C contains p.
     """
@@ -9,46 +9,48 @@ def is_in_circumcircle(C: tuple[int|float, int|float, int|float], p: tuple[int|f
     dy = p[1]*Ca-Cp[1]
     return dx*dx + dy*dy <= Cr2
 
-def circumcircle_of_triangle(D: list[tuple[int|float, int|float]]):
+def circumcircle_of_triangle(a: tuple[float, float], b:tuple[float, float], c:tuple[float, float]) -> tuple[float, tuple[float, float], float]:
     """
-        Given that the circumcircle of P is centered at (cx, cy) and r, returns (a, (a*cx, a*cy), (a*r)^2).
-        When all points of P are integer points, then all three values will be integers.
-        
-        `a` will always be positive.
+        Given a triangle abc, compute the circumcircle C of it.
+        C will be given as a format (Ca, Cp, Cr2), where, given that the circumcircle is centered at (x, y) with radius r:
+
+        - Ca > 0
+        - Cp = Ca * (x, y)
+        - Cr2 = (Ca * r) ** 2
+
+        When all of `a`, `b`, `c` are integers, then all Ca, Cp, Cr2 will be integers.
+
+        When three points are colinear, Ca will be zero.
     """
 
-    assert(len(D) <= 3)
+    ax, ay = a
+    bx, by = b
+    cx, cy = c
 
-    if len(D) == 1: return (1, D[0], 0)
-    if len(D) == 2:
-        ax, ay = D[0]
-        bx, by = D[1]
+    Ca = _determinant(ax, ay, 1, bx, by, 1, cx, cy, 1)
+    al, bl, cl = ax*ax+ay*ay, bx*bx+by*by, cx*cx+cy*cy
+
+    Cb = _determinant(ax, ay, al, bx, by, bl, cx, cy, cl)
+    Cx = _determinant(al, ay, 1, bl, by, 1, cl, cy, 1)
+    Cy = _determinant(ax, al, 1, bx, bl, 1, cx, cl, 1)
+
+    if Ca < 0: Ca, Cb, Cx, Cy = -Ca, -Cb, -Cx, -Cy
+
+    return (2*Ca, (Cx, Cy), Cx*Cx+Cy*Cy + 4*Ca*Cb)
+
+def _min_enclosing_circle_inner(P: list[tuple[float, float]], i: int, D: list[tuple[float, float]]) -> tuple[float, tuple[float, float], float]:
+    if i == len(P):
+        assert(1 <= len(D) <= 2)
+        if len(D) == 1: return (1, D[0], 0)
+        ax, ay = D[0]; bx, by = D[1]
         dx, dy = ax-bx, ay-by
         return (2, (ax+bx, ay+by), dx*dx + dy*dy)
-    
-    A, B, C = D
-    ax, ay = A
-    bx, by = B
-    cx, cy = C
-    
-    a = _determinant(ax, ay, 1, bx, by, 1, cx, cy, 1)
-    if a == 0: return circumcircle_of_triangle([min(D), max(D)])
-
-    l2A, l2B, l2C = ax*ax+ay*ay, bx*bx+by*by, cx*cx+cy*cy
-    b = _determinant(ax, ay, l2A, bx, by, l2B, cx, cy, l2C)
-    Sx = _determinant(l2A, ay, 1, l2B, by, 1, l2C, cy, 1)
-    Sy = _determinant(ax, l2A, 1, bx, l2B, 1, cx, l2C, 1)
-
-    if a < 0: a, b, Sx, Sy = -a, -b, -Sx, -Sy
-
-    return (2*a, (Sx, Sy), Sx*Sx+Sy*Sy + 4*a*b)
-
-def _min_enclosing_circle_inner(P: list[tuple[int|float, int|float]], i: int, D: list[tuple[int|float, int|float]]) -> tuple[int|float, int|float, int|float]:
-    if i == len(P) or len(D) == 3: return circumcircle_of_triangle(D)
-    if len(D) == 0 and i+1 >= len(P): return circumcircle_of_triangle(P[i:])
+    if len(D) == 0 and i == len(P)-1: return (1, P[i], 0)
 
     C = _min_enclosing_circle_inner(P, i+1, D)
     if is_in_circumcircle(C, P[i]): return C
+
+    if len(D) == 2: return circumcircle_of_triangle(P[i], D[0], D[1])
 
     D.append(P[i])
     C = _min_enclosing_circle_inner(P, i+1, D)
@@ -56,11 +58,16 @@ def _min_enclosing_circle_inner(P: list[tuple[int|float, int|float]], i: int, D:
 
     return C
 
-def min_enclosing_circle(P: list[tuple[int|float, int|float]]):
+def min_enclosing_circle(P: list[tuple[float, float]]) -> tuple[float, tuple[float, float], float]:
     """
-        Given that the circumcircle of P is centered at (cx, cy) and r, returns (a, (a*cx, a*cy), (a*r)^2).
-        When all points of P are integer points, then all three values will be integers.
-        
-        Note that `a` might be a negative number.
+        Given a list of points P, returns the minimal enclosing circle C.
+        C will be given as a format (Ca, Cp, Cr2), where, given that the circumcircle is centered at (x, y) with radius r:
+
+        - Ca > 0
+        - Cp = Ca * (x, y)
+        - Cr2 = (Ca * r) ** 2
+
+        When all points are integral points, then all Ca, Cp, Cr2 will be integers.
     """
+    assert(len(P) > 0)
     return _min_enclosing_circle_inner(P, 0, [])
