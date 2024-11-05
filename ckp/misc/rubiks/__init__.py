3edd5403a7e255ae6282c6c93bc5963e27feb8e4
Jimin Park <pjm950@gmail.com>
2024-11-05 16:09:34 +0900

    Minor clarifications.

--- ckp/misc/rubiks/__init__.py
+++ ckp/misc/rubiks/__init__.py
@@ -13,11 +13,11 @@
     ## Cube Move
 
     A move consists of an integer.
-    - Bits 0-1: \# of quarter turns.
-      - Clockwise (w.r.t. layer direction) when layer \# is even, counter-clockwise when layer \# is odd
+    - Bits 0-1: Amount of quarter turns.
+      - Clockwise (w.r.t. layer direction) when layer number is even, counter-clockwise when layer number is odd
     - Bits 2-3: Layer direction.
       - 0=U, 1=F, 2=R
-    - Rest bits: Layer \#.
+    - Rest bits: Layer number.
       - For example, when layer direction is 0, layer 0 is U, and layer 1 is D.
 
     U, D, F, B, R, L are 1, 17, 5, 21, 9, 25 respectively.
