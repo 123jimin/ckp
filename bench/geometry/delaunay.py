cecb5a1a2fe24a3945ca5cfd5e36e5d9e31250a3
Jimin Park <pjm950@gmail.com>
2024-11-06 20:21:33 +0900

    Changed delaunay benchmark to run profiler instead.

--- bench/geometry/delaunay.py
+++ bench/geometry/delaunay.py
@@ -1,16 +1,24 @@
+import cProfile
 from bench.util import bench
 from ckp.geometry.delaunay import delaunay_triangulation
 
 import random
 random.seed(42)
 
-N = 100_000
+N = 10_000
 points = [(random.randrange(1_000_000_000), random.randrange(1_000_000_000)) for _ in range(N)]
 
 def bench_delaunay_triangulation():
-    return max(delaunay_triangulation(points))
+    res = sum(1 for _ in delaunay_triangulation(points))
+    assert(res == 29977)
+
+    return res
 
 if __name__ == '__main__':
+    cProfile.run("[bench_delaunay_triangulation() for _ in range(5)]", sort='tottime')
+
+    """
     bench([
         "bench_delaunay_triangulation()"
     ], num_trials=8, global_vars=globals())
+    """
