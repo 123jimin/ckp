85fea0aa5b039a68203393f650bcc7e1b53053b8
Jimin Park <pjm950@gmail.com>
2024-11-07 02:40:20 +0900

    Separated edge and face next.

--- ckp/geometry/delaunay/mesh.py
+++ ckp/geometry/delaunay/mesh.py
@@ -1,7 +1,7 @@
 """
-    A quarter-edge is just an integer x.
-    - Quad mask indices: x//4
-    - Src and dst indices: x//2
+    A quarter-edge is just an integer x. An edge and its dual(rot) shares the same integer.
+    - Quad mask indices: x//2
+    - Src and dst indices: x and x^1
     - Next indices: x
 """
 
@@ -14,8 +14,6 @@ class DelaunayMesh:
         'shuffled_indices',
         '_quad_mask',
         'quad_src',
-        'quad_dst',
-        'quad_nxt',
         'quad_nxt_edge',
         'quad_nxt_face',
     )
@@ -25,10 +23,7 @@ class DelaunayMesh:
 
     _quad_mask: list[bool] # To mark deleted edges (`False`)
     quad_src: list[int] # Source vertex indices for a quarter-edge
-    quad_dst: list[int] # Destination vertex indices for a quarter-edge
-    quad_nxt: list[int] # Next quarter-edge indices
     
-    # TODO
     quad_nxt_edge: list[int] # Next edge quarter-edge indices
     quad_nxt_face: list[int] # Next facequarter-edge indices
 
@@ -43,8 +38,6 @@ class DelaunayMesh:
         
         self._quad_mask = []
         self.quad_src = []
-        self.quad_dst = []
-        self.quad_nxt = []
 
         self.quad_nxt_edge = []
         self.quad_nxt_face = []
@@ -60,46 +53,43 @@ class DelaunayMesh:
                 yield (quad_src[i], quad_src[i+1])
     
     def _lnext(self, x: int) -> int:
-        x = self.quad_nxt[x^((x^(x+3))&3)]
-        return x^((x^(x+1))&3)
+        return self.quad_nxt_face[x^1]^1
     
     def _lprev(self, x: int) -> int:
-        x = self.quad_nxt[x^((x^(x+1))&3)]
-        return x^((x^(x+3))&3)
+        return self.quad_nxt_face[x]
     
     def _oprev(self, x: int) -> int:
-        x = self.quad_nxt[x^((x^(x+1))&3)]
-        return x^((x^(x+1))&3)
+        return self.quad_nxt_face[x]^1
     
     def make_edge(self, src: int, dst: int) -> int:
+        x = len(self.quad_src)
+
         self._quad_mask.append(True)
         self.quad_src.extend((src, dst))
-        self.quad_dst.extend((dst, src))
+        self.quad_nxt_edge.extend((x, x+1))
+        self.quad_nxt_face.extend((x+1, x))
         
-        x = len(self.quad_nxt)
-        self.quad_nxt.extend((x, x+3, x+2, x+1))
-
         return x
     
     def splice_edge(self, x: int, y: int):
-        quad_nxt = self.quad_nxt
-        xf = quad_nxt[x]; xf = xf^((xf^(xf+1))&3)
-        yf = quad_nxt[y]; yf = yf^((yf^(yf+1))&3)
-        quad_nxt[xf], quad_nxt[yf] = quad_nxt[yf], quad_nxt[xf]
-        quad_nxt[x], quad_nxt[y] = quad_nxt[y], quad_nxt[x]
+        nxt_edge, nxt_face = self.quad_nxt_edge, self.quad_nxt_face
+        
+        xf = nxt_edge[x]; yf = nxt_edge[y]
+        nxt_face[xf], nxt_face[yf] = nxt_face[yf], nxt_face[xf]
+        nxt_edge[x], nxt_edge[y] = nxt_edge[y], nxt_edge[x]
     
     def delete_edge(self, edge: int):
-        assert(self._quad_mask[edge//4])
+        assert(self._quad_mask[edge//2])
 
         self.splice_edge(edge, self._oprev(edge))
-        self.splice_edge(edge^2, self._oprev(edge^2))
-        self._quad_mask[edge//4] = False
+        self.splice_edge(edge^1, self._oprev(edge^1))
+        self._quad_mask[edge//2] = False
 
     def connect(self, x: int, y: int) -> int:
         """ Connect two edges sharing the same left-face by creating a new quad-edge. """
-        z = self.make_edge(self.quad_dst[x//2], self.quad_src[y//2])
+        z = self.make_edge(self.quad_src[x^1], self.quad_src[y])
         self.splice_edge(z, self._lnext(x))
-        self.splice_edge(z^2, y)
+        self.splice_edge(z^1, y)
         return z
     
     def _triangulate(self, start: int, end: int) -> tuple[int, int]:
@@ -112,21 +102,21 @@ class DelaunayMesh:
                 i1, i2, i3 = start, start+1, start+2
                 a = self.make_edge(i1, i2)
                 b = self.make_edge(i2, i3)
-                self.splice_edge(a^2, b)
+                self.splice_edge(a^1, b)
                 ori = vec2_orientation(*vertices[start:end])
                 if ori < 0:
                     c = self.connect(b, a)
-                    return [c^2, c]
+                    return [c^1, c]
                 if ori > 0:
                     c = self.connect(b, a)
-                return [a, b^2]
+                return [a, b^1]
             else:
                 # 1 or 2 vertices
                 a = self.make_edge(start, end-1)
-                return [a, a^2]
+                return [a, a^1]
         
         # 4 or more vertices
-        src, dst, nxt = self.quad_src, self.quad_dst, self.quad_nxt
+        src, nxt = self.quad_src, self.quad_nxt_edge
         mid = (start + end) // 2
         
         ldo, ldi = self._triangulate(start, mid)
@@ -134,56 +124,56 @@ class DelaunayMesh:
 
         # Compute the lower common tangent of left and right halves.
         while True:
-            ldi_src, rdi_src = vertices[src[ldi//2]], vertices[src[rdi//2]]
-            if vec2_orientation(rdi_src, ldi_src, vertices[dst[ldi//2]]) > 0: ldi = self._lnext(ldi)
-            elif vec2_orientation(ldi_src, vertices[dst[rdi//2]], rdi_src) > 0: rdi = nxt[rdi^2]
+            ldi_src, rdi_src = vertices[src[ldi]], vertices[src[rdi]]
+            if vec2_orientation(rdi_src, ldi_src, vertices[src[ldi^1]]) > 0: ldi = self._lnext(ldi)
+            elif vec2_orientation(ldi_src, vertices[src[rdi^1]], rdi_src) > 0: rdi = nxt[rdi^1]
             else: break
 
-        base1 = self.connect(rdi^2, ldi)
-        if src[ldi//2] == src[ldo//2]: ldo = base1^2
-        if src[rdi//2] == src[rdo//2]: rdo = base1
+        base1 = self.connect(rdi^1, ldi)
+        if src[ldi] == src[ldo]: ldo = base1^1
+        if src[rdi] == src[rdo]: rdo = base1
 
         while True:
-            base1_src = vertices[src[base1//2]]
-            base1_dst = vertices[dst[base1//2]]
+            base1_src = vertices[src[base1]]
+            base1_dst = vertices[src[base1^1]]
 
-            lcand = nxt[base1^2]
-            if vec2_orientation(base1_dst, base1_src, vertices[dst[lcand//2]]) > 0:
-                while is_in_circumcircle_of_triangle(base1_dst, base1_src, vertices[dst[lcand//2]], vertices[dst[nxt[lcand]//2]]):
+            lcand = nxt[base1^1]
+            if vec2_orientation(base1_dst, base1_src, vertices[src[lcand^1]]) > 0:
+                while is_in_circumcircle_of_triangle(base1_dst, base1_src, vertices[src[lcand^1]], vertices[src[nxt[lcand]^1]]):
                     t = nxt[lcand]
                     self.delete_edge(lcand)
                     lcand = t
             
             rcand = self._oprev(base1)
-            if vec2_orientation(base1_dst, base1_src, vertices[dst[rcand//2]]) > 0:
-                while is_in_circumcircle_of_triangle(base1_dst, base1_src, vertices[dst[rcand//2]], vertices[dst[self._oprev(rcand)//2]]):
+            if vec2_orientation(base1_dst, base1_src, vertices[src[rcand^1]]) > 0:
+                while is_in_circumcircle_of_triangle(base1_dst, base1_src, vertices[src[rcand^1]], vertices[src[self._oprev(rcand)^1]]):
                     t = self._oprev(rcand)
                     self.delete_edge(rcand)
                     rcand = t
 
-            lvalid = vec2_orientation(base1_dst, base1_src, vertices[dst[lcand//2]]) > 0
-            rvalid = vec2_orientation(base1_dst, base1_src, vertices[dst[rcand//2]]) > 0
+            lvalid = vec2_orientation(base1_dst, base1_src, vertices[src[lcand^1]]) > 0
+            rvalid = vec2_orientation(base1_dst, base1_src, vertices[src[rcand^1]]) > 0
 
             if not lvalid and not rvalid: break
 
             if (
                 not lvalid or
-                rvalid and is_in_circumcircle_of_triangle(vertices[dst[lcand//2]], vertices[src[lcand//2]], vertices[src[rcand//2]], vertices[dst[rcand//2]])
-            ): base1 = self.connect(rcand, base1^2)
-            else: base1 = self.connect(base1^2, lcand^2)
+                rvalid and is_in_circumcircle_of_triangle(vertices[src[lcand^1]], vertices[src[lcand]], vertices[src[rcand]], vertices[src[rcand^1]])
+            ): base1 = self.connect(rcand, base1^1)
+            else: base1 = self.connect(base1^1, lcand^1)
 
         last = end-1
 
-        while src[ldo//2] != start: ldo = nxt[ldo^2]
-        while src[rdo//2] != last: rdo = self._lprev(rdo)
+        while src[ldo] != start: ldo = nxt[ldo^1]
+        while src[rdo] != last: rdo = self._lprev(rdo)
 
         return [ldo, rdo]
 
     def _prune(self):
         quad_mask = self._quad_mask
         self.quad_src = [x for (i, x) in enumerate(self.quad_src) if quad_mask[i//2]]
-        self.quad_dst = [x for (i, x) in enumerate(self.quad_dst) if quad_mask[i//2]]
-        self.quad_nxt = [x for (i, x) in enumerate(self.quad_nxt) if quad_mask[i//4]]
+        self.quad_nxt_edge = [x for (i, x) in enumerate(self.quad_nxt_edge) if quad_mask[i//2]]
+        self.quad_nxt_face = [x for (i, x) in enumerate(self.quad_nxt_face) if quad_mask[i//2]]
         self._quad_mask = [x for x in quad_mask if x]
     
     def triangulate(self):
