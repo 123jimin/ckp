40092c5227581befa16d5e08a84f51ba411d23ae
Jimin Park <pjm950@gmail.com>
2025-06-15 19:23:25 +0900

    Improved performance of LIS.

--- ckp/misc/lis.py
+++ ckp/misc/lis.py
@@ -8,14 +8,14 @@ def lis(A: list):
 
     if not A: return []
 
-    L = [0]
+    L = []
+    B = []
     P = [None] * len(A)
 
-    for i in range(1, len(A)):
-        if j := bisect_right(L, A[i], key=lambda v: A[v]):
-            P[i] = L[j-1]
-        if j == len(L): L.append(i)
-        else: L[j] = i
+    for i, x in enumerate(A):
+        if j := bisect_right(B, x): P[i] = L[j-1]
+        if j == len(L): L.append(i); B.append(x)
+        else: L[j] = i; B[j] = x
 
     v = L[-1]
     V = []
