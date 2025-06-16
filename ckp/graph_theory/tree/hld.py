142372b560646846a566375401b4823ffbfd038b
Jimin Park <pjm950@gmail.com>
2025-06-16 12:17:55 +0900

    Implemented non-recursive HLD initialization.

--- ckp/graph_theory/tree/hld.py
+++ ckp/graph_theory/tree/hld.py
@@ -12,47 +12,50 @@ class TreeHLDData:
     
     def __str__(self): return f"<TreeHLD path_index={self.path_index} paths={self.paths}>"
 
-def _tree_hld_init_node(hld: TreeHLDData, sizes: list[int], ind: int, curr: int, parent: int = -1):
-    paths = hld.paths
-    hld.path_index[curr] = (ind, len(curr_path := paths[ind]))
-    curr_path.append(curr)
-
-    neighbors = hld.tree.neighbors[curr]
-
-    if parent == -1:
-        if len(neighbors) == 0: return
-    else:
-        if len(neighbors) == 1: return
-        if len(neighbors) == 2:
-            for ch in neighbors:
-                if ch != parent:
-                    _tree_hld_init_node(hld, sizes, ind, ch, curr)
-                    return
-            assert(False)
-
-    max_ch_size = max(sizes[ch] for ch in neighbors if ch != parent)
-
-    for ch in neighbors:
-        if ch == parent: continue
-        if sizes[ch] == max_ch_size:
-            # Heavy Path
-            max_ch_size += 1
-            _tree_hld_init_node(hld, sizes, ind, ch, curr)
-        else:
-            # Light Path
-            new_ind = len(paths)
-            paths.append([])
-            _tree_hld_init_node(hld, sizes, new_ind, ch, curr)
-
 def tree_hld_init(tree: TreeData, sizes: list[int]|None = None):
-    if sizes is None: sizes = tree_sizes(tree.neighbors, tree.root)
+    tree_neighbors = tree.neighbors
+    if sizes is None: sizes = tree_sizes(tree_neighbors, tree.root)
 
+    path_index = [None] * len(tree)
+    paths = [[]]
+
+    stack = [(0, tree.root, -1)]
+    while stack:
+        curr_ind, curr, parent = stack.pop()
+        path_index[curr] = (curr_ind, len(curr_path := paths[curr_ind]))
+        curr_path.append(curr)
+        neighbors = tree_neighbors[curr]
+
+        if parent == -1:
+            if len(neighbors) == 0: continue
+        else:
+            if len(neighbors) == 1: continue
+            if len(neighbors) == 2:
+                for ch in neighbors:
+                    if ch != parent:
+                        stack.append((curr_ind, ch, curr))
+                        break
+                else:
+                    assert(False)
+                continue
+        
+        max_ch_size = max(sizes[ch] for ch in neighbors if ch != parent)
+        for ch in neighbors:
+            if ch == parent: continue
+            if sizes[ch] == max_ch_size:
+                # Heavy Path
+                max_ch_size += 1
+                stack.append((curr_ind, ch, curr))
+            else:
+                # Light Path
+                new_ind = len(paths)
+                paths.append([])
+                stack.append((new_ind, ch, curr))
+    
     hld = TreeHLDData()
     hld.tree = tree
-    hld.path_index = [None] * len(tree)
-    hld.paths = [[]]
-
-    _tree_hld_init_node(hld, sizes, 0, tree.root)
+    hld.path_index = path_index
+    hld.paths = paths
 
     return hld
 
@@ -60,7 +63,7 @@ def tree_hld_decompose_descendant(hld: TreeHLDData, node: int, ancestor: int):
     """
         Given that `ancestor` is an ancestor of `node`, returns a list of intervals in heavy paths (`hld.paths`).
         - Each interval is given as a tuple `(path_index, path_start, path_end)`, which represents an interval `hld.paths[path_index][path_start:path_end]`.
-        - Intervals are non-overlaping.
+        - Intervals are non-overlapping.
         - The union of all intervals is a continuous path from `node` to `ancestor`, including both ends.
     """
     
