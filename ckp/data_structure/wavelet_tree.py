5f5ba32f9b0189041c5834be8fa65915a5231858
Jimin Park <pjm950@gmail.com>
2025-06-22 01:15:38 +0900

    Added naive implementation of WaveletTree.

--- ckp/data_structure/wavelet_tree.py
+++ ckp/data_structure/wavelet_tree.py
@@ -1,4 +1,4 @@
-# TODO: Implement this!
+import bisect
 
 class LinkedWaveletTreeData:
     """ A wavelet tree that stores left and right children via references. """
@@ -8,11 +8,36 @@ class LinkedWaveletTreeData:
     _min: int
     _max: int
 
-    def __init__(self):
-        pass
+    def __init__(self, arr: list[int], min_value: int, max_value: list[int]):
+        self._arr, self._min, self._max = arr, min_value, max_value
+        self._left = self._right = None
+
+        if min_value == max_value: return
+
+        half_value = (min_value + max_value) // 2
+        assert(min_value <= half_value < max_value)
+
+        left_arr = list(filter(lambda v: v <= half_value, arr))
+        right_arr = list(filter(lambda v: v > half_value, arr))
+
+        assert(left_arr and right_arr)
+        self._left = LinkedWaveletTreeData(left_arr, min_value, half_value)
+        self._right = LinkedWaveletTreeData(right_arr, half_value+1, max_value)
+
+    def __str__(self): return self._str()
+
+    def _str(self, indent: int = 0):
+        lines = [f"{' '*indent}{self._arr}"]
+        if self._left: lines.append(self._left._str(indent+2))
+        if self._right: lines.append(self._right._str(indent+2))
+
+        return '\n'.join(lines)
+
+    def __repr__(self): return f"LinkedWaveletTreeData(arr={repr(self._arr)}, min={self._min}, max={self._max})"
 
 def linked_wavelet_tree_init(arr: list[int]):
-    pass
+    min_value, max_value = min(arr), max(arr)
+    return LinkedWaveletTreeData(arr, min_value, max_value)
 
 def linked_wavelet_tree_map_left(tree: LinkedWaveletTreeData, index: int) -> int:
     pass
--- test/data_structure/wavelet_tree.py
+++ test/data_structure/wavelet_tree.py
@@ -1,2 +1,28 @@
 import unittest
 from ckp.data_structure.wavelet_tree import *
+
+class TestLinkedWaveletTree(unittest.TestCase):    
+    def _assert_tree(self, node: LinkedWaveletTreeData, arr: list[int]):
+        self.assertEqual(node._min, min(arr))
+        self.assertEqual(node._max, max(arr))
+        self.assertListEqual(node._arr, arr)
+
+        if node._min == node._max:
+            self.assertIsNone(node._left)
+            self.assertIsNone(node._right)
+            return
+    
+        self.assertIsNotNone(node._left)
+        self.assertIsNotNone(node._right)
+
+        self.assertEqual(node._left._min, node._min)
+        self.assertEqual(node._left._max + 1, node._right._min)
+        self.assertEqual(node._right._max, node._max)
+
+        self._assert_tree(node._left, [x for x in arr if node._left._min <= x <= node._left._max])
+        self._assert_tree(node._right, [x for x in arr if node._right._min <= x <= node._right._max])
+
+    def test_paper(self):
+        arr = [3, 3, 9, 1, 2, 1, 7, 6, 4, 8, 9, 4, 3, 7, 5, 9, 2, 7, 3, 5, 1, 3]
+        tree = linked_wavelet_tree_init(arr)
+        self._assert_tree(tree, arr)
