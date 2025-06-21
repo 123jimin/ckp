0437250beccf520333591e2b5629494d10def67e
Jimin Park <pjm950@gmail.com>
2025-06-22 00:52:40 +0900

    Created a faster version.

--- bench/acmicpc/p17103.py
+++ bench/acmicpc/p17103.py
@@ -5,12 +5,14 @@ V = [random.randint(450_000, 500_000)*2 for _ in range(100)]
 
 def bench():
     sieve = prime_sieve_init(1_000_000)
+    primes = {p: 1 for p in prime_sieve_primes(sieve)}
+
     ans = 0
     for v in V:
         v2 = v // 2
-        for p in prime_sieve_primes(sieve):
+        for p in primes:
             if p > v2: break
-            ans += prime_sieve_query(sieve, v-p)
-    assert(ans == 578969)
+            ans += (v-p) in primes
+    assert(ans == 578977)
 
 tags = {'number_theory', 'primality_test', 'prime_sieve'}
