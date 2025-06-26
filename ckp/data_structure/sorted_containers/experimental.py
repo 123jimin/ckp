604474477273d1480b03fc88aa6021b925d4d2c4
Jimin Park <pjm950@gmail.com>
2025-06-26 19:25:00 +0900

    Added skeleton for experimental, alternative SortedList.

---
+++ ckp/data_structure/sorted_containers/experimental.py
@@ -0,0 +1,25 @@
+# Alternative implementation of sorted list.
+
+from collections.abc import MutableSequence
+
+class SortedList(MutableSequence):
+    def __init__(self, iterable=None):
+        pass
+
+    def clear(self):
+        pass
+
+    def add(self, value):
+        pass
+
+    def update(self, iterable):
+        pass
+
+    def __contains__(self, value):
+        pass
+
+    def discard(self, value):
+        pass
+
+    def remove(self, value):
+        pass
