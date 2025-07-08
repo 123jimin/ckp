f358ce54c52cfefdc6d41dab9197c7fbbcda4107
Jimin Park <pjm950@gmail.com>
2025-07-08 17:44:39 +0900

    Added thorough testing for `is_prime` and `PrimeSieve`.

--- test/number_theory/prime_sieve.py
+++ test/number_theory/prime_sieve.py
@@ -2,7 +2,7 @@ import unittest
 from ckp.number_theory.prime_sieve import *
 
 from collections import Counter
-from ckp.number_theory.primality_test import is_prime_naive
+from ckp.number_theory.primality_test import is_prime, is_prime_naive
 
 class TestPrimeSieve(unittest.TestCase):
     def test_single_alloc(self):
@@ -23,14 +23,36 @@ class TestPrimeSieve(unittest.TestCase):
 
         for n in range(100001): self.assertEqual(is_prime_naive(n), sieve.is_prime(n), f"primality test for {n=}")
 
+    def test_is_prime_large(self):
+        sieve = PrimeSieve(1_000_000)
+        for n in range(3, 1_000_001, 2): self.assertEqual(is_prime(n), sieve.is_prime(n), f"primality test for {n=}")
+
+    @unittest.skip("stress test")
+    def test_is_prime_large(self):
+        sieve = PrimeSieve(10_000_000)
+        for n in range(3, 10_000_001, 2): self.assertEqual(is_prime(n), sieve.is_prime(n), f"primality test for {n=}")
+
     def test_factor(self):
         sieve = PrimeSieve(100000)
         self.assertEqual(Counter(sieve.factor(42)), Counter([2, 3, 7]))
         self.assertEqual(Counter(sieve.factor(16384)), Counter([2]*14))
     
     def test_example(self):
+        sieve = PrimeSieve(10**6)
+        self.assertEqual(len(list(sieve.primes())), 78498)
+        
+        self.assertTrue(sieve.is_prime(999983))
+        self.assertFalse(sieve.is_prime(999991))
+        self.assertEqual(len(list(sieve.primes())), 78498)
+
+        self.assertTrue(sieve.is_prime(1010129))
+        self.assertEqual(len(list(sieve.primes())), 79257)
+
+    @unittest.skip("stress test")
+    def test_example_large(self):
         sieve = PrimeSieve(10**7)
         self.assertEqual(len(list(sieve.primes())), 664579)
+        self.assertEqual(sum(sieve.primes()), 3203324994356)
 
         self.assertTrue(sieve.is_prime(9999991))
         self.assertFalse(sieve.is_prime(9999997))
