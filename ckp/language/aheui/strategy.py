bbec986cf1641619d9772e2f93d07992ae62a993
Jimin Park <pjm950@gmail.com>
2025-07-11 13:45:41 +0900

    Updated aheui strategy.

--- ckp/language/aheui/strategy.py
+++ ckp/language/aheui/strategy.py
@@ -43,6 +43,11 @@ def aheui_generate_small_optimal_codes() -> list[dict[int, str]]:
     return optimal_codes
 
 AHEUI_SMALL_OPTIMALS = aheui_generate_small_optimal_codes()
+AHEUI_DIV_OPPORTUNITY = {
+    k//d: k
+    for k in AHEUI_SMALL_OPTIMALS[2].keys()
+    for d in range(2, 10)
+}
 
 @cache
 def aheui_push_nonneg_int_naive(n: int) -> str:
@@ -113,7 +118,7 @@ def aheui_push_int_from_trivial(top: int, value: int) -> str:
     if 2 <= (delta := value-top) <= 9: return AHEUI_NUMERALS[delta] + "다"
     if 2 <= (delta := top-value) <= 9: return AHEUI_NUMERALS[delta] + "타"
     if value%top == 0 and 2 <= (d := value//top) <= 9: return AHEUI_NUMERALS[d] + "따"
-    if top%value == 0 and 2 <= (d := top//value) <= 9: return AHEUI_NUMERALS[d] + "나"
+    if 2 <= (d := top//value) <= 9 and top//d == value: return AHEUI_NUMERALS[d] + "나"
 
     ## len == 3 ##
     if 2 <= (n := top+value) <= 9: return AHEUI_NUMERALS[n] + "파타"
@@ -142,6 +147,10 @@ def _aheui_push_int_get_strategies(func: Callable[[int|None, int, int], str], to
 
     yield func(top, value)
 
+    for d in range(2, 10):
+        yield func(top, value-d, 2) + AHEUI_NUMERALS[d] + "다"
+        yield func(top, value+d, 2) + AHEUI_NUMERALS[d] + "타"
+
     yield func(None, value-top) + "다"
     yield "빠" + func(top, value-top, 2) + "다"
 
@@ -152,18 +161,31 @@ def _aheui_push_int_get_strategies(func: Callable[[int|None, int, int], str], to
         yield func(None, value//top) + "따"
         yield "빠" + func(top, value//top) + "따"
     else:
-        yield func(None, value//top) + "따" + func((value//top)*top, value)
-        yield "빠" + func(top, value//top) + "따" + func((value//top)*top, value)
+        yield func(None, value//top) + "따" + func((value//top)*top, value, 2)
+        yield "빠" + func(top, value//top) + "따" + func((value//top)*top, value, 2)
 
-        yield func(None, 1+value//top) + "따" + func((1+value//top)*top, value)
-        yield "빠" + func(top, 1+value//top) + "따" + func((1+value//top)*top, value)
+        yield func(None, 1+value//top) + "따" + func((1+value//top)*top, value, 2)
+        yield "빠" + func(top, 1+value//top) + "따" + func((1+value//top)*top, value, 2)
 
-    if value >= 100:
-        value_sqrt = math.isqrt(value)
+    if abs(value) >= 100:
+        value_sqrt = math.isqrt(abs(value))
         for i in range(10):
-            target = value_sqrt - i
-            if value%target == 0:
-                yield func(top, target) + "빠" + func(target, value//target) + "따"
+            target_0 = value_sqrt - i
+            target_1 = value//target_0
+            mul = target_0*target_1
+            rest = func(mul, value, 2)
+
+            yield func(top, target_0) + "빠" + func(target_0, target_1, 1 if value > 0 else 2) + "따" + rest
+            yield func(top, target_1) + "빠" + func(target_1, target_0, 1 if value > 0 else 2) + "따" + rest
+
+            if target_1 == target_0 + 1:
+                yield func(top, target_0) + "빠빠따다" + rest
+                yield func(top, target_1) + "빠빠따파타" + rest
+
+    if value >= 500 and (target := AHEUI_DIV_OPPORTUNITY.get(value)):
+        d = target // value
+        assert(target // d == value)
+        yield func(top, target) + func(None, d) + "나"
 
     yield "빠따" + func(top*top, value)
 
@@ -187,9 +209,9 @@ def aheui_push_int_from(top: int|None, value: int, effort: int = 0) -> str:
     next_func = lambda top, value, delta=1: aheui_push_int_from(top, value, new_effort) if (new_effort:=effort-delta) >= 0 else aheui_push_int_from_naive(top, value)
     return min(_aheui_push_int_get_strategies(next_func, top, value), key=len)
 
-# Minimal and maximal values where the function is optimal between them.
+# Minimal and maximal values where the function is optimal between them, for `effort == 2`.
 aheui_push_int_from.min_optimal = -587
-aheui_push_int_from.max_optimal = 532
+aheui_push_int_from.max_optimal = 1000
 
 def aheui_from_vals(arr: list[int]) -> list[str]:
     effort = 2
