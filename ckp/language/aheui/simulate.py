0d21b9decf42d7988fee3bcc266ac916144e16fa
Jimin Park <pjm950@gmail.com>
2025-07-11 13:01:24 +0900

    More effective heuristics.

--- ckp/language/aheui/simulate.py
+++ ckp/language/aheui/simulate.py
@@ -17,6 +17,7 @@ def aheui_run_strip(code: str) -> str:
             case '나': assert(len(stack) >= 2); y, x = stack.pop(), stack.pop(); stack.append(x // y)
             case '라': assert(len(stack) >= 2); y, x = stack.pop(), stack.pop(); stack.append(x % y)
             case '빠': assert(len(stack) >= 1); stack.append(stack[-1])
+            case '파': assert(len(stack) >= 2); stack[-1], stack[-2] = stack[-2], stack[-1]
             case '마': assert(len(stack) >= 1); stack.pop()
             case '망': assert(len(stack) >= 1); output.append(stack.pop())
             case '맣': assert(len(stack) >= 1); output.append(chr(stack.pop()))
--- ckp/language/aheui/strategy.py
+++ ckp/language/aheui/strategy.py
@@ -1,93 +1,206 @@
 """ Strategy to print shortest possible Aheui code. """
 
 from functools import cache
+import math, itertools
+from typing import Callable
 
 AHEUI_NUMERALS = ('반반타', '발밤타', '반', '받', '밤', '발', '밦', '밝', '밣', '밞')
 
-@cache
-def aheui_push_int_naive(n: int) -> str:
-    """ Returns any "good enough" Aheui code that pushes `n` onto an empty stack. """
-    if n < 0: return "반" + aheui_push_int_naive(2-n) + "타"
+def aheui_generate_small_optimal_codes() -> list[dict[int, str]]:
+    """ Creates a database of optimal Aheui codes for small integers, for code length 1/3/5. """
+    optimal_codes: list[dict[int, str]] = [
+        {i: AHEUI_NUMERALS[i] for i in range(2, 10)},
+        len_3 := dict(),
+        len_5 := dict(),
+    ]
 
-    if n < len(AHEUI_NUMERALS): return AHEUI_NUMERALS[n]
-    if n <= 2*(len(AHEUI_NUMERALS)-1):
-        m = n // 2
-        return AHEUI_NUMERALS[m] + AHEUI_NUMERALS[n-m] + "다"
+    for i in range(2, 10):
+        for j in range(2, 10):
+            for (c, k) in (("다", i+j), ("따", i*j), ("타", i-j)):
+                if 2 <= k <= 9: continue
+                if k in len_3: continue
+                len_3[k] = AHEUI_NUMERALS[i] + AHEUI_NUMERALS[j] + c
     
+    for (v, code) in len_3.items():
+        if v < 10: continue
+        v_sq = v*v
+        if v_sq in len_3: continue
+        len_5[v_sq] = code + "빠따"
+
+    for i in range(2, 10):
+        i_code = AHEUI_NUMERALS[i]
+        for (j, j_code) in len_3.items():
+            for (new_code, k) in (
+                (i_code + j_code + "다", i+j),
+                (i_code + j_code + "따", i*j),
+                (i_code + j_code + "타", i-j),
+                (j_code + i_code + "타", j-i),
+            ):
+                if 2 <= k <= 9: continue
+                if k in len_3 or k in len_5: continue
+                len_5[k] = new_code
+
+    return optimal_codes
+
+AHEUI_SMALL_OPTIMALS = aheui_generate_small_optimal_codes()
+
+@cache
+def aheui_push_nonneg_int_naive(n: int) -> str:
+    assert(n >= 0)
+
+    for small_optimals in AHEUI_SMALL_OPTIMALS:
+        if (code := small_optimals.get(n)): return code
+
+        for i in range(9, 1, -1):
+            if (code := small_optimals.get(n-i)): return code + AHEUI_NUMERALS[i] + "다"
+            if (code := small_optimals.get(n+i)): return code + AHEUI_NUMERALS[i] + "타"
+    
+    n_sqrt = math.isqrt(n)
+    if n_sqrt*n_sqrt == n: return aheui_push_nonneg_int_naive(n_sqrt) + "빠따"
+
     for d in range(9, 1, -1):
-        if n%d == 0: return AHEUI_NUMERALS[d] + aheui_push_int_naive(n // d) + "따"
+        if n%d == 0: return AHEUI_NUMERALS[d] + aheui_push_nonneg_int_naive(n // d) + "따"
     
     if n%9 < 2 and n%8 >= 2:
-        return AHEUI_NUMERALS[n%8] + AHEUI_NUMERALS[8] + aheui_push_int_naive(n//8) + "따다"
+        return AHEUI_NUMERALS[n%8] + AHEUI_NUMERALS[8] + aheui_push_nonneg_int_naive(n//8) + "따다"
     
-    return AHEUI_NUMERALS[n%9] + AHEUI_NUMERALS[9] + aheui_push_int_naive(n//9) + "따다"
+    return AHEUI_NUMERALS[n%9] + AHEUI_NUMERALS[9] + aheui_push_nonneg_int_naive(n//9) + "따다"
 
 @cache
-def aheui_make_naive_from(top: int|None, value: int) -> str:
-    if top is None: return min((AHEUI_NUMERALS[top] + aheui_make_naive_from(top, value) for top in range(2, 10)), key=len)
+def aheui_push_neg_int_naive(n: int) -> str:
+    assert(n < 0)
+    
+    for small_optimals in AHEUI_SMALL_OPTIMALS:
+        if (code := small_optimals.get(n)): return code
+
+        for i in range(9, 1, -1):
+            if (code := small_optimals.get(i-n)): return AHEUI_NUMERALS[i] + code + "타"
+    
+    for d in range(9, 1, -1):
+        if n%d == 0: return AHEUI_NUMERALS[d] + aheui_push_neg_int_naive(n // d) + "따"
+
+    return "반" + aheui_push_nonneg_int_naive(2-n) + "타"
+
+def aheui_push_int_naive(n: int) -> str:
+    """
+        Returns any "good enough" Aheui code that pushes `n` onto an empty stack.
+    """
+    if n < 0: return aheui_push_neg_int_naive(n)
+    else: return aheui_push_nonneg_int_naive(n)
+
+# Minimal and maximal values where the function is optimal between them.
+aheui_push_int_naive.min_optimal = -418
+aheui_push_int_naive.max_optimal = 459
+
+def aheui_push_int_from_trivial(top: int, value: int) -> str:
+    """ Returns trivially optimal code that pushes `value` onto a stack, replacing `top`. """
+
+    ## len == 0 ##
     if top == value: return ""
+
+    # No further improvements possible for top == 0
+    if not top:
+        for optimals in AHEUI_SMALL_OPTIMALS:
+            if (code := optimals.get(value)): return "마" + code
+        return None
+    
+    ## len == 2 ##
+    if 2 <= value <= 9: return "마" + AHEUI_NUMERALS[value]
     if not value: return "빠타"
     if value == 1: return "빠나"
-    if 2 <= value <= 9: return "마" + AHEUI_NUMERALS[value]
     if top*top == value: return "빠따"
 
-    candidates = []
-    if value > top:
-        candidates.append(aheui_push_int_naive(value - top) + "다")
+    if 2 <= (delta := value-top) <= 9: return AHEUI_NUMERALS[delta] + "다"
+    if 2 <= (delta := top-value) <= 9: return AHEUI_NUMERALS[delta] + "타"
+    if value%top == 0 and 2 <= (d := value//top) <= 9: return AHEUI_NUMERALS[d] + "따"
+    if top%value == 0 and 2 <= (d := top//value) <= 9: return AHEUI_NUMERALS[d] + "나"
 
-        if value % top == 0:
-            candidates.append(aheui_push_int_naive(value // top) + "따")
-        else:
-            candidates.append(aheui_push_int_naive(value // top) + "따" + aheui_push_int_naive(value % top) + "다")
-            candidates.append(aheui_push_int_naive(1 + value // top) + "따" + aheui_push_int_naive(top - value % top) + "타")
+    ## len == 3 ##
+    if 2 <= (n := top+value) <= 9: return AHEUI_NUMERALS[n] + "파타"
+    if 2 <= (n := top*value) <= 9: return AHEUI_NUMERALS[n] + "파나"
+
+    return None
+
+def aheui_push_int_from_naive(top: int|None, value: int) -> str:
+    if top is None: return aheui_push_int_naive(value)
+    if (code := aheui_push_int_from_trivial(top, value)) is not None: return code
+
+    # Only attempt the simplest strategies.
+    return min(
+        "마" + aheui_push_int_naive(value),
+        aheui_push_int_naive(value-top) + "다",
+        aheui_push_int_naive(top-value) + "타",
+        key=len
+    )
+
+def _aheui_push_int_get_strategies(func: Callable[[int|None, int, int], str], top: int, value: int):
+    # Assume that (top, value) is not trivial.
+
+    yield "마" + func(None, value)
+    if not top: return
+    if not value: return
+
+    yield func(top, value)
+
+    yield func(None, value-top) + "다"
+    yield "빠" + func(top, value-top, 2) + "다"
+
+    yield func(None, top-value) + "타"
+    yield "빠" + func(top, top-value, 2) + "타"
+
+    if value%top == 0:
+        yield func(None, value//top) + "따"
+        yield "빠" + func(top, value//top) + "따"
     else:
-        candidates.append(aheui_push_int_naive(top - value) + "타")
+        yield func(None, value//top) + "따" + func((value//top)*top, value)
+        yield "빠" + func(top, value//top) + "따" + func((value//top)*top, value)
 
-    return min(candidates, key=len)
+        yield func(None, 1+value//top) + "따" + func((1+value//top)*top, value)
+        yield "빠" + func(top, 1+value//top) + "따" + func((1+value//top)*top, value)
+
+    if value >= 100:
+        value_sqrt = math.isqrt(value)
+        for i in range(10):
+            target = value_sqrt - i
+            if value%target == 0:
+                yield func(top, target) + "빠" + func(target, value//target) + "따"
+
+    yield "빠따" + func(top*top, value)
 
 @cache
-def aheui_make_from(top: int|None, value: int, effort: int = 0) -> str:
+def aheui_push_int_from(top: int|None, value: int, effort: int = 0) -> str:
     if top is None:
-        if 0 <= value < 200: return aheui_make_naive_from(None, value)
-        return min((AHEUI_NUMERALS[top] + aheui_make_from(top, value, effort) for top in range(2, 10)), key=len)
-    if top == value: return ""
-    if not value: return "빠타"
-    if value == 1: return "빠나"
-    if 2 <= value <= 9: return "마" + AHEUI_NUMERALS[value]
-    if 2 <= value-top <= 9: return AHEUI_NUMERALS[value-top] + "다"
-    if 2 <= top-value <= 9: return AHEUI_NUMERALS[top-value] + "타"
-    if value%top == 0 and 2 <= value//top <= 9: return AHEUI_NUMERALS[value//top] + "따"
-    if top*top == value: return "빠따"
-    
-    if not effort: return aheui_make_naive_from(top, value)
+        for optimals in AHEUI_SMALL_OPTIMALS:
+            if (code := optimals.get(value)): return code
 
-    candidates = [aheui_make_from(top, value, effort-1)]
-    if value > top:
-        candidates.append(aheui_make_from(None, value-top, effort-1) + "다")
+        naive_code = aheui_push_int_naive(value)
+        if aheui_push_int_naive.min_optimal <= value <= aheui_push_int_naive.max_optimal: return naive_code
 
-        if value % top == 0:
-            candidates.append(aheui_make_from(None, value//top, effort-1) + "따")
-            candidates.append("빠" + aheui_make_from(top, value//top, effort-1) + "따")
-        else:
-            candidates.append(aheui_make_from(None, value//top, effort-1) + "따" + aheui_make_from(top*(value//top), value, effort-1))
-            candidates.append(aheui_make_from(None, 1 + value//top, effort-1) + "따" + aheui_make_from(top*(value//top+1), value, effort-1))
-            
-            candidates.append("빠" + aheui_make_from(top, value//top, effort-1) + "따" + aheui_make_from(top*(value//top), value, effort-1))
-            candidates.append("빠" + aheui_make_from(top, 1 + value//top, effort-1) + "따" + aheui_make_from(top*(value//top+1), value, effort-1))
-    else:
-        candidates.append(aheui_make_from(None, top-value, effort-1) + "타")
+        return min(
+            naive_code,
+            *(AHEUI_NUMERALS[top] + aheui_push_int_from(top, value, effort) for top in range(2, 10)),
+            key=len
+        )
 
-    return min(candidates, key=len)
+    if (code := aheui_push_int_from_trivial(top, value)) is not None: return code
+
+    next_func = lambda top, value, delta=1: aheui_push_int_from(top, value, new_effort) if (new_effort:=effort-delta) >= 0 else aheui_push_int_from_naive(top, value)
+    return min(_aheui_push_int_get_strategies(next_func, top, value), key=len)
+
+# Minimal and maximal values where the function is optimal between them.
+aheui_push_int_from.min_optimal = -587
+aheui_push_int_from.max_optimal = 532
 
 def aheui_from_vals(arr: list[int]) -> list[str]:
+    effort = 2
     prev_val = None
     result: list[str] = []
     for x in arr:
         if prev_val is None:
-            result.append(aheui_make_from(None, x, 4))
+            result.append(aheui_push_int_from(None, x, effort))
         else:
-            from_none = aheui_make_from(None, x, 4)
-            from_prev = aheui_make_from(prev_val, x, 4)
+            from_none = aheui_push_int_from(None, x, effort)
+            from_prev = aheui_push_int_from(prev_val, x, effort)
             if len(from_none) < len(from_prev)+1:
                 result.append(from_none)
             else:
